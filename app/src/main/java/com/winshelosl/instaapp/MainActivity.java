c06aa419dad791f8f9d6b28bae256860a927cad6
Winshelo <winshelo@hotmail.com>
2022-08-21 19:38:01 -0400

    Initial commit

---
+++ app/src/main/java/com/winshelosl/instaapp/MainActivity.java
@@ -0,0 +1,82 @@
+package com.winshelosl.instaapp;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+
+import com.winshelosl.instaapp.databinding.ActivityMainBinding;
+
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+
+public class MainActivity extends AppCompatActivity {
+
+   ActivityMainBinding binding;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(R.layout.activity_main);
+        setContentView(binding.getRoot());
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        // Sets the Toolbar to act as the ActionBar for this Activity window.
+        // Make sure the toolbar exists in the activity and is not null
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
+
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item->{
+
+            switch (item.getItemId()) {
+
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.addPic:
+                    replaceFragment(new AddPicFragment());
+
+                    break;
+                case R.id.Person:
+                    replaceFragment(new PersonFragment());
+                    break;
+
+            }
+            return  true;
+        });
+
+    }
+    private void replaceFragment(Fragment fragment){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.frameLayout, fragment);
+                fragmentTransaction.commit();
+
+
+    }
+
+
+    // Menu icons are inflated just as they were with actionbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.top_navmenu, menu);
+        return true;
+    }
+
+}
